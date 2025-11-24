4f15b8c1b2174add1ef588f98ecef643712c9b52
Backlink Generator Tool – Free SEO Backlink Builder <backlink.generator.tool@gmail.com>
2025-11-24 18:12:36 +0100

    Update backlink-generator.js

--- backlink-generator.js
+++ backlink-generator.js
@@ -929,7 +929,11 @@ window.addEventListener('DOMContentLoaded', async () => {
             urlInput.value = norm;
             startRun();
         } else {
-            alert('Invalid URL');
+            //alert('Invalid URL');
+            
+            const here = window.location.href.split('#')[0];
+            const testUrl = here + (here.includes('?') ? '&' : '?') + here;
+            setExternalLink("Open Test", testUrl);
         }
     } else {
         const here = window.location.href.split('#')[0];
